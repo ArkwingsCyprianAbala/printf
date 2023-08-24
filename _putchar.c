f68100d6475175f546f2033b539d51c841649832
Cyprian Abala <arkwingscyprianabala@gmail.com>
2023-08-24 15:20:54 -0700

    myPrintf function

--- _putchar.c
+++ _putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
+ * _putchar - write character c to stdout
  * @pucr: print a character
  * @c: character input
- * Return: 1 on success
+ * Return: 1 on success or else -1
  */
 int pucr(char c)
 {
